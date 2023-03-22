bcc8bc797ee388bedd7d84671fc30d93b0288e2b
jrx <jerome+git@rouaix.eu>
2023-03-23 00:52:34 +0100

    more tests & validations

--- src/directory.rs
+++ src/directory.rs
@@ -2,6 +2,7 @@ use include_dir::*;
 use regex::Regex;
 use serde::{Deserialize, Serialize};
 use std::{borrow::Cow, collections::HashMap};
+use validator::Validate;
 
 static DIRECTORY_FILES: Dir = include_dir!("$DIRECTORY_DATA_FOLDER");
 
@@ -21,6 +22,56 @@ pub enum DirectoryError {
   FileNameAndKeyDoNotMatch(String, String),
   #[error("File name or key does not match conventions (only lowercase alphanumeric characters) : {0}")]
   ShouldMatchNamingConventions(String),
+}
+
+#[derive(Clone, Debug, Validate)]
+pub struct Directory {
+  pub tags: HashMap<String, Tag>,
+  pub items: HashMap<String, Item>,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq, Validate)]
+pub struct Tag {
+  #[serde(default)]
+  pub key: String,
+  pub title: Cow<'static, str>,
+  pub description: Vec<Cow<'static, str>>,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq, Validate)]
+pub struct Item {
+  #[serde(default)]
+  pub key: String,
+  pub name: Cow<'static, str>,
+  pub title: Cow<'static, str>,
+  pub tags: Vec<Cow<'static, str>>,
+  pub created_in: usize,
+  pub concluded_in: Option<usize>,
+  pub description: Vec<Cow<'static, str>>,
+  pub url: url::Url,
+  pub backlink: Option<url::Url>,
+
+  #[serde(default)]
+  pub links: Vec<DirectoryLink>,
+
+  #[serde(default)]
+  pub events: Vec<DirectoryEvent>,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq, Validate)]
+pub struct DirectoryLink {
+  pub target_key: String,
+  pub begin_in: usize,
+  pub end_in: Option<usize>,
+  #[validate(length(min = 20))]
+  pub description: Cow<'static, str>,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq, Validate)]
+pub struct DirectoryEvent {
+  pub happened_in: usize,
+  #[validate(length(min = 20))]
+  pub description: Cow<'static, str>,
 }
 
 fn is_yaml(file: &File) -> bool {
@@ -69,7 +120,8 @@ pub fn load_directory() -> Result<Directory, DirectoryError> {
     }
     let yaml = tag_file.contents_utf8().ok_or(DirectoryError::CouldNotReadFile)?;
     let mut tag: Tag = serde_yaml::from_str(yaml).map_err(|e| {
-      tracing::error!("tag deserialization : `{:?}`", e);
+      let file_name = tag_file.path().to_string_lossy();
+      tracing::error!("{file_name} tag deserialization error : `{e:?}`");
       DirectoryError::YamlDeserialization
     })?;
 
@@ -92,7 +144,8 @@ pub fn load_directory() -> Result<Directory, DirectoryError> {
     }
     let yaml = item_file.contents_utf8().ok_or(DirectoryError::CouldNotReadFile)?;
     let mut item: Item = serde_yaml::from_str(yaml).map_err(|e| {
-      tracing::error!("item deserialization : `{:?}`", e);
+      let file_name = item_file.path().to_string_lossy();
+      tracing::error!("{file_name} item deserialization error : `{e:?}`");
       DirectoryError::YamlDeserialization
     })?;
 
@@ -113,105 +166,10 @@ pub fn load_directory() -> Result<Directory, DirectoryError> {
   Ok(directory)
 }
 
-#[derive(Clone, Debug)]
-pub struct Directory {
-  pub tags: HashMap<String, Tag>,
-  pub items: HashMap<String, Item>,
-}
-
-#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
-pub struct Tag {
-  #[serde(default)]
-  pub key: String,
-  pub title: Cow<'static, str>,
-  pub description: Vec<Cow<'static, str>>,
-}
-
-#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
-pub struct Item {
-  #[serde(default)]
-  pub key: String,
-  pub name: Cow<'static, str>,
-  pub title: Cow<'static, str>,
-  pub tags: Vec<Cow<'static, str>>,
-  pub created_in: Option<usize>,
-  pub concluded_in: Option<usize>,
-  pub description: Vec<Cow<'static, str>>,
-  pub url: url::Url,
-  pub backlink: Option<url::Url>,
-
-  #[serde(default)]
-  pub links: Vec<DirectoryLink>,
-
-  #[serde(default)]
-  pub events: Vec<DirectoryEvent>,
-}
-
-#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
-pub struct DirectoryLink {
-  pub target_key: String,
-  pub begin_in: Option<usize>,
-  pub end_in: Option<usize>,
-  pub description: Cow<'static, str>,
-}
-
-#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
-pub struct DirectoryEvent {
-  pub happened_in: usize,
-  pub description: Cow<'static, str>,
-}
-
 #[cfg(test)]
 mod tests {
   use super::*;
   use std::collections::HashSet;
-
-  pub fn validate_directory(directory: &Directory) -> anyhow::Result<()> {
-    let mut keys = HashSet::new();
-    for key in directory.tags.keys() {
-      if !keys.insert(key.clone()) {
-        anyhow::bail!("Key `{key}` is not unique accross tags & items, you should not have multiple {key}.yaml or {key}.yml files")
-      }
-    }
-    for key in directory.items.keys() {
-      if !keys.insert(key.clone()) {
-        anyhow::bail!("Key `{key}` is not unique accross tags & items, you should not have multiple {key}.yaml or {key}.yml files")
-      }
-    }
-
-    let mut fail = false;
-
-    for key in directory.tags.keys() {
-      tracing::info!("- tag : `{}`", key);
-      let mut found = false;
-      for item in directory.items.values() {
-        if item.tags.iter().any(|x| x.as_ref() == key.as_str()) {
-          found = true;
-          break;
-        }
-      }
-      if !found {
-        tracing::error!("tag `{}` not found in any item", key);
-        fail = true;
-      }
-    }
-
-    for (key, item) in &directory.items {
-      tracing::info!("- item : `{}`", key);
-      for tag in &item.tags {
-        if !directory.tags.contains_key(tag.as_ref()) {
-          tracing::error!("tag not found : `{}` in item `{}`", tag, key);
-          fail = true;
-        }
-      }
-    }
-
-    if fail {
-      anyhow::bail!("Directory did not validate, see logs !")
-    }
-
-    Ok(())
-  }
 
   #[test]
   pub fn validate_current_directory_files() -> anyhow::Result<()> {
@@ -246,6 +204,85 @@ mod tests {
       check_filename_and_key(&File::new("pl0p", &[]), "plop"),
       Err(DirectoryError::FileNameAndKeyDoNotMatch(file, key)) if file == "pl0p" && key == "plop"
     ));
+    Ok(())
+  }
+
+  pub fn validate_directory(directory: &Directory) -> anyhow::Result<()> {
+    let mut fail = false;
+
+    let mut keys = HashSet::new();
+    for key in directory.tags.keys() {
+      if !keys.insert(key.clone()) {
+        tracing::error!("Key `{key}` is not unique accross tags & items, you should not have multiple {key}.yaml or {key}.yml files");
+        fail = true;
+      }
+    }
+    for key in directory.items.keys() {
+      if !keys.insert(key.clone()) {
+        tracing::error!("Key `{key}` is not unique accross tags & items, you should not have multiple {key}.yaml or {key}.yml files");
+        fail = true;
+      }
+    }
+
+    for (tag_key, tag) in &directory.tags {
+      tracing::info!("- tag : `{}`", tag_key);
+      let found = directory
+        .items
+        .values()
+        .flat_map(|x| x.tags.iter())
+        .any(|x| x.as_ref() == tag_key.as_str());
+
+      if !found {
+        tracing::error!("tag `{}` not found in any item", tag_key);
+        fail = true;
+      }
+
+      if tag.description.is_empty() {
+        tracing::error!("tag `{}` has no description", tag_key);
+        fail = true;
+      }
+    }
+
+    for (item_key, item) in &directory.items {
+      tracing::info!("- item : `{}`", item_key);
+      for tag in &item.tags {
+        if !directory.tags.contains_key(tag.as_ref()) {
+          tracing::error!("tag not found : `{}` in item `{}`", tag, item_key);
+          fail = true;
+        }
+      }
+
+      if item.description.is_empty() {
+        tracing::error!("item `{}` has no description", item_key);
+        fail = true;
+      }
+
+      for link in &item.links {
+        if !directory.items.contains_key(&link.target_key) {
+          tracing::error!("link target not found : `{}` in item `{}`", link.target_key, item_key);
+          fail = true;
+        }
+        if link.target_key == item.key {
+          tracing::error!("link target is the same as the item : `{}` in item `{}`", link.target_key, item_key);
+          fail = true;
+        }
+        if link.description.is_empty() {
+          tracing::error!("link description is empty : `{}` in item `{}`", link.target_key, item_key);
+          fail = true;
+        }
+      }
+
+      for event in &item.events {
+        if event.description.is_empty() {
+          tracing::error!("event description is empty : `{}` in item `{}`", event.description, item_key);
+          fail = true;
+        }
+      }
+    }
+
+    if fail {
+      anyhow::bail!("Directory did not validate, see logs !")
+    }
 
     Ok(())
   }
@@ -260,11 +297,11 @@ mod tests {
       Item {
         key: "wymmo".into(),
         name: "Wymmo".into(),
-        title: "Wymmo".into(),
+        title: "Où voudriez-vous habiter ?".into(),
         tags: vec!["b2b".into()],
-        created_in: Some(2005),
+        created_in: 2005,
         concluded_in: None,
-        description: vec![],
+        description: vec!["Où !! Mais où ! Parles !".into()],
         url: "https://wymmo.com".parse()?,
         backlink: None,
         links: vec![],
@@ -278,12 +315,17 @@ mod tests {
         name: "Vendre n'importe quoi, n'importe comment !".into(),
         title: "LBC".into(),
         tags: vec!["b2b".into()],
-        created_in: Some(2006),
+        created_in: 2006,
         concluded_in: None,
-        description: vec![],
+        description: vec!["Le Bon Canard".into()],
         url: "https://wymmo.com".parse()?,
         backlink: None,
-        links: vec![],
+        links: vec![DirectoryLink {
+          target_key: "wymmo".into(),
+          begin_in: 2026,
+          end_in: None,
+          description: "Wymmo rachète LBC, ils supportaient pas la compétition".into(),
+        }],
         events: vec![],
       },
     );
@@ -294,23 +336,57 @@ mod tests {
 
     assert!(matches!(validate_directory(&directory), Ok(())));
 
+    // empty item description is forbidden
+    let mut wrong_directory = directory.clone();
+    wrong_directory.tags.entry("b2b".into()).and_modify(|x| {
+      x.description = vec![];
+    });
+    assert!(matches!(validate_directory(&wrong_directory), Err(_)));
+
+    // empty tag description is forbidden
+    let mut wrong_directory = directory.clone();
+    wrong_directory.items.entry("wymmo".into()).and_modify(|x| {
+      x.description = vec![];
+    });
+    assert!(matches!(validate_directory(&wrong_directory), Err(_)));
+
+    // adds a tag with the same key than an item
     let mut wrong_directory = directory.clone();
     wrong_directory
       .tags
-      .insert("wymmo".into(), Tag { key: "wymmo".into(), title: "Wymmo".into(), description: vec![] });
+      .insert("wymmo".into(), Tag { key: "wymmo".into(), title: "Wymmo".into(), description: vec!["description".into()] });
     assert!(matches!(validate_directory(&wrong_directory), Err(_)));
 
+    // adds a tag with a key that is use by no item
     let mut wrong_directory = directory.clone();
-    wrong_directory
-      .tags
-      .insert("not_used".to_string(), Tag { key: "not_used".into(), title: "This tag is not used".into(), description: vec![] });
+    wrong_directory.tags.insert(
+      "not_used".to_string(),
+      Tag { key: "not_used".into(), title: "This tag is not used".into(), description: vec!["description".into()] },
+    );
     assert!(matches!(validate_directory(&wrong_directory), Err(_)));
 
+    // adds a non exising tag to an item
     let mut wrong_directory = directory.clone();
     wrong_directory
       .items
       .entry("wymmo".into())
       .and_modify(|x| x.tags.push("not_existing_tag".into()));
+    assert!(matches!(validate_directory(&wrong_directory), Err(_)));
+
+    // adds a link to itself
+    let mut wrong_directory = directory.clone();
+    wrong_directory.items.entry("wymmo".into()).and_modify(|x| {
+      x.links
+        .push(DirectoryLink { target_key: "wymmo".into(), begin_in: 2020, end_in: None, description: "description".into() })
+    });
+    assert!(matches!(validate_directory(&wrong_directory), Err(_)));
+
+    // adds a link to an item that does not exist
+    let mut wrong_directory = directory.clone();
+    wrong_directory.items.entry("wymmo".into()).and_modify(|x| {
+      x.links
+        .push(DirectoryLink { target_key: "does_not_exists".into(), begin_in: 2020, end_in: None, description: "description".into() })
+    });
     assert!(matches!(validate_directory(&wrong_directory), Err(_)));
 
     Ok(())
