dc250f93a6b4749b8da6b2de8afb23f2a59d0b26
jrx <jerome+git@rouaix.eu>
2023-04-10 22:50:55 +0200

    build_data_json test can output json

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,35 @@ mod directory;
 use crate::directory::*;
 
 #[allow(dead_code)]
-fn _a_fonction_to_user_everything() -> Result<(), DirectoryError> {
+fn _a_fonction_to_use_everything() -> Result<(), DirectoryError> {
   let _directory = load_directory()?;
   Ok(())
+}
+
+//tests
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use serde::Serialize;
+
+  #[derive(Debug, Serialize)]
+  pub struct DirectoryGraphData {
+    #[serde(rename = "tags")]
+    pub tags: Vec<Tag>,
+    #[serde(rename = "items")]
+    pub items: Vec<Item>,
+  }
+
+  #[ignore]
+  #[test]
+  fn build_data_json() -> anyhow::Result<()> {
+    let directory = load_directory()?;
+    let directory_data =
+      DirectoryGraphData { tags: directory.tags.values().cloned().collect(), items: directory.items.values().cloned().collect() };
+    let data_json_path = format!("{}/data.json", env!("CARGO_MANIFEST_DIR"));
+
+    let data_json = serde_json::to_string_pretty(&directory_data)?;
+    std::fs::write(data_json_path, data_json)?;
+    Ok(())
+  }
 }
